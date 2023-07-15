4726066af4873f637909edd7508adc0ac7e19f18
IrinaJ <irina.ul87@gmail.com>
2023-07-15 10:05:30 -0400

    missing modules

--- frontend/src/socketio-frontend.ts
+++ frontend/src/socketio-frontend.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3001";
+const URL = "https://quizzable-a90200121f06.herokuapp.com/";
 
 export const clientSocketInstance = io(URL);
 
