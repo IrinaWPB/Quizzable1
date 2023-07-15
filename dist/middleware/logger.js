70396faa67ded57d0d5dd7054bd248082c1b44f1
IrinaJ <irina.ul87@gmail.com>
2023-07-15 09:45:42 -0400

    new js

--- dist/middleware/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-//for develpment; to be deleted later
-function logger(req, res, next) {
-    console.log(`RECEIVED ${req.method} request to ${req.path}. ${req.path} is next to visit after middleware is run`);
-    return next();
-}
-exports.logger = logger;
